07e2c2a90386e6e52bfaa543423c965b424d4d28
Developer <dev@gaborbuzasi.com>
2019-11-16 17:28:57 +0000

    add clickable elements

--- autorun.js
+++ autorun.js
@@ -1,12 +1,55 @@
 let path = window.location.href;
 
+let trustedList = [
+    "facebook.com"
+];
+
+let blockedList = [
+    "instagram.com"
+];
+
 const websiteDomain = "https://yourwebsitesname.net";
 
-if (path.includes("gmail") || path.includes("mail"))
-{
-    window.open(websiteDomain + '?d=gmail', '_blank');
+window.onload = function () {
+    var anchorTags = document.getElementsByTagName('a');
+
+    for(i = 0; i < anchorTags.length; i++)
+    {
+        let anchor = anchorTags[i];
+        
+        if (trustedList.some(t => anchor.href.toLowerCase().includes(t.toLowerCase()))) 
+        {
+            let sp = document.createElement('a');
+            sp.setAttribute('title', 'Open minigame')
+            sp.setAttribute('href', 'randomgamenotreadyyet');
+            sp.setAttribute('style', 'text-decoration: none;');
+            sp.setAttribute('target', '_blank');
+            sp.innerHTML = '🐤';
+
+            anchor.parentNode.appendChild(sp);
+        }
+        else if (blockedList.some(b => anchor.href.toLowerCase().includes(b.toLowerCase())))
+        {
+            anchor.href = '#';
+            let sp = document.createElement('a');
+            sp.setAttribute('title', 'Open minigame')
+            sp.setAttribute('href', 'randomgamenotreadyyet');
+            sp.setAttribute('style', 'text-decoration: none;');
+            sp.setAttribute('target', '_blank');
+            
+            // Google uses ping to redirect to page
+            sp.setAttribute('ping', '#');
+            sp.innerHTML = '🦞';
+
+            anchor.parentNode.appendChild(sp);
+        }
+    }
 }
-else if (path.includes("facebook"))
-{
-    window.open(websiteDomain + '?d=facebook', '_blank');
-}
+// if (path.includes("gmail") || path.includes("mail"))
+// {
+//     window.open(websiteDomain + '?d=gmail', '_blank');
+// }
+// else if (path.includes("facebook"))
+// {
+//     window.open(websiteDomain + '?d=facebook', '_blank');
+// }
