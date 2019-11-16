75bb5851480e9919d8690fc7bceb4ea174d2337d
gyevnarb <gyevnarb@gmail.com>
2019-11-16 23:31:34 +0000

    Added start tile

--- games/UnsafeNet/UnsafeNet/Assets/Scripts/QuestionPanelController.cs
+++ games/UnsafeNet/UnsafeNet/Assets/Scripts/QuestionPanelController.cs
@@ -1,29 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class QuestionPanelController : MonoBehaviour
 {
     private Canvas canvas;
+    private string[] questionTexts;
 
     // Start is called before the first frame update
     void Start()
     {
         canvas = GetComponent<Canvas>();
-
         canvas.enabled = false;
     }
 
-    public void DisplayQuestion(Interactable tile)
+    public void StartQuestion(string[] texts)
     {
-        //transform.GetChild(0).GetComponent<Text>().text = tile.questionText;
-        //transform.GetChild(1).GetComponent<Text>().text = 
+        questionTexts = texts;
+        Enable();
+
+        int i = 0;
+        foreach (var child in transform.GetComponentsInChildren<Text>())
+        {
+            child.text = questionTexts[i++];
+        }
+    }
+
+
+    public void EnableButtons()
+    {
+        foreach (var item in transform.GetComponentsInChildren<Button>())
+        {
+            item.enabled = true;
+            item.GetComponentInParent<Image>().enabled = true;
+        }
+    }
+
+    public void DisableButtons()
+    {
+        foreach (var item in transform.GetComponentsInChildren<Button>())
+        {
+            item.enabled = false;
+            item.GetComponentInParent<Image>().enabled = false;
+            item.GetComponentInChildren<Text>().text = "";
+        }
+    }
+
+    public void SetTitle(string text)
+    {
+        GetComponentInChildren<Text>().text = text;
     }
 
     public void Enable()
     {
         canvas.enabled = true;
+        EnableButtons();
     }
 
     public void Disable()
